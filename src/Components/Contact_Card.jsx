065400bceeb7cfeec03e4c249a3818fba0e18c20
SabauCristi <sabau_cristi@yahoo.com>
2024-03-16 03:02:07 +0200

    Another small fix

--- src/Components/Contact_Card.jsx
+++ src/Components/Contact_Card.jsx
@@ -11,17 +11,17 @@ function Contact_Card(){
                 <div className="flex flex-col items-center justify-center bg-secondary">
                     <p className="text-primary text-center text-2xl w-4/5 pt-4 md:text-3xl">Suntem la un singur mesaj distanță!</p>
                     <p className="text-primary text-center text-lg w-4/5 pt-4 md:pt-6 md:text-xl">Echipa noastră este mereu bucuroasă să te ajute și să îți ofere soluții personalizate.</p>
-                    <div className="grid grid-rows-3 mt-6 gap-3 w-4/5 md:w-[90%] md:gap-6 lg:mt-10 lg:gap-10 mb-6">
+                    <div className="grid grid-rows-3 mt-6 gap-3 w-[90%] md:w-[90%] md:gap-6 lg:mt-10 lg:gap-10 mb-6">
                         <div className="flex flex-row items-center gap-3 md:gap-6">
-                            <img src={phone_icon} className="aspect-square border-2 border-brick p-2 overflow-visible rounded-full w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-3"></img>
+                            <img src={phone_icon} className="aspect-square border-2 border-brick p-3 overflow-visible rounded-full w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-3"></img>
                             <p className="text-primary text-2xl text-start pb-1 font-normal md:text-2xl">0773 957 912</p>
                         </div>
                         <div className="flex flex-row items-center gap-3 md:gap-6">
-                            <img src={email_icon} className="aspect-square border-2 border-brick p-2 rounded-full overflow-visible w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-4"></img>
+                            <img src={email_icon} className="aspect-square border-2 border-brick p-3 rounded-full overflow-visible w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-4"></img>
                             <p className="text-primary text-2xl text-start pb-1 font-normal md:text-2xl">office@derasovil.ro</p>
                         </div>
                         <div className="flex flex-row items-center gap-3 md:gap-6">
-                            <img src={location_icon} className="aspect-square border-2 border-brick p-2 rounded-full w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-4"></img>
+                            <img src={location_icon} className="aspect-square border-2 border-brick p-3 rounded-full w-[15%] md:p-4 md:w-[10%] lg:p-3 lg:w-[15%] xl:p-4"></img>
                             <p className="text-primary text-2xl text-start pb-1 font-normal md:text-2xl">Strada 8, Numarul 9, Oșorhei, Județul Bihor</p>
                         </div>
                     </div>
